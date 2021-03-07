ad06f09b53d361d44d5a283e0002d6cee75d5356
Gr1ffor <54276416+Gr1ffor@users.noreply.github.com>
2021-03-07 16:32:38 -0300

    Seek DOido

---
+++ UnityToonShader-master/Assets/Exercicio1.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Exercicio1 : MonoBehaviour
+{
+
+    public Transform target;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position += (target.position - transform.position).normalized * 3f * Time.deltaTime;
+    }
+}
---
+++ UnityToonShader-master/Assets/Exercicio4.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Exercicio4 : MonoBehaviour
+{
+    [SerializeField] [Range(0f, 5f)] float lerpTime;
+    [SerializeField] Vector3[] positions;
+    int posIndex = 0; int length; float p = 0f;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        length = positions.Length;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = Vector3.Lerp(transform.position, positions[posIndex], lerpTime * Time.deltaTime);
+        p = Mathf.Lerp(p, 1f, lerpTime * Time.deltaTime);
+        if (p > .9f)
+        {
+            p = 0f;
+            posIndex++;
+            posIndex = (posIndex >= length) ? 0 : posIndex;
+        }
+
+    }
+}
